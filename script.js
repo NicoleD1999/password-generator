1c7b41689f1419f8741ca08af038ccef9f09a491
NicoleD1999 <nmd1018@yahoo.com>
2022-03-23 18:37:08 -0500

    moved if statements outside of function

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
+//Variables 
+var passwordLength
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"]
+var useNumbers = ["1","2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowercaseLetter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var selectedChar =[]
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Write password to the #password input
-function userInput (){
-  var passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
+// Create new function to store user input
+function userInput () {
+  passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
   console.log(passwordLength)
-  if (passwordLength<8 || passwordLength>128)
+  if (passwordLength<8 || passwordLength>128) {
     alert ("please enter a number between 8 & 128")
     return null
   } 
-  else if () {
+  specialCharacters = confirm("Click ok if you want to use special characters. exp. (! ,@ , # ,$)");
 
-  }
-  var specialCharacters = confirm("Click ok if you want to use special characters. exp. (! ,@ , # ,$)");
-  if (specialCharacters=true) {
+  useNumbers = confirm("Click ok if you want to use numbers.");
 
-  }
-  var useNumbers = confirm("Click ok if you want to use numbers.");
-  if (useNumbers=true) {
+  capitalLetters = confirm("Click ok if you want to use capital letters.");
 
-  }
-  var capitalLetters = confirm("Click ok if you want to use capital letters.");
-  if (capitalLetters=true) {
+  lowercaseLetter = confirm("click ok if you want to us lowercase letters.");
 
-  }
-  var lowercaseLetter = confirm("click ok if you want to us lowercase letters.");
-  if (lowercaseLetter=true) {
-
-  }
 }
-function generatePassword ();
+if (specialCharacters && useNumbers)
+specialCharacters=specialCharacters.concat(useNumbers)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+function generatePassword () {
+  userInput();
+}
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
