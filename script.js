a792e69a7923b44029ab6e659f8bfbc68192d979
NicoleD1999 <nmd1018@yahoo.com>
2022-03-23 09:40:32 -0500

    added prompt and confirm boxes, assigned generatePassword as a function

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
+function generatePassword (){
+  prompt("How many characters would you like your password to be?");
+  confirm("Do you want to use any special charaters? exp. (! ,@ , # ,$)");
+  confirm("Do you want to use any numbers?");
+  confirm("Do you want to use any capital letters?");
+  confirm("Do you want to use any lowercase letters?");
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
