1cd35276c7a2ac3f6b08708ed599e843f0eb9588
NicoleD1999 <nmd1018@yahoo.com>
2022-03-23 14:45:42 -0500

    assigned prompt to variable

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
-function generatePassword (){
-  prompt("How many characters would you like your password to be?");
-  confirm("Do you want to use any special charaters? exp. (! ,@ , # ,$)");
+function userInput (){
+  var passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
+  console.log(passwordLength)
+  if (passwordLength<8){
+    alert ("please enter a number between 8 & 130")
+    return null
+  }
+  var specialCharacters = confirm("Do you want to use any special charaters? exp. (! ,@ , # ,$)");
   confirm("Do you want to use any numbers?");
   confirm("Do you want to use any capital letters?");
   confirm("Do you want to use any lowercase letters?");
